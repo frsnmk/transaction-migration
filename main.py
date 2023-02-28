18c168b1d7c24ba94f336656ee97adff5883a89f
frsnmk <razedman@gmail.com>
2023-02-28 17:33:16 +0700

    Create model (datasource) for penjualan Oktober 2022

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import os
 import pandas as pd
 
 from dotenv import load_dotenv
+from datasource.selling import Selling
 
 load_dotenv()
+
+path = os.getenv('LAPORAN_KEUANGAN_PATH')
+
+selling = Selling(path).data_penjualan()
